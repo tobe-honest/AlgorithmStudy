d789ae8d5ebde91689ce3e1cb238e5892e8b7289
채오이 <41178045+chae52@users.noreply.github.com>
2022-09-02 11:28:53 +0900

    Create 1992_쿼드트리.py

---
+++ chaewon/week1/1992_쿼드트리.py
@@ -0,0 +1,37 @@
+n= int(input())
+graph = []
+for i in range(n):
+    x=input()
+    temp=list()
+    for xx in x:
+        temp.append(int(xx))
+    graph.append(temp)
+
+answer=''
+def divide(x,y,length):
+    global answer
+
+    match=graph[x][y]
+    is_matched=True
+    for i in range(x,x+length):
+        for j in range(y,y+length):
+            if graph[i][j]!=match:
+                is_matched=False
+                break
+
+    if is_matched:
+        answer+=str(match)
+    else:
+        points=[]
+        answer+='('
+        points.append((x,y))
+        points.append((x,y+length//2))
+        points.append((x+length//2,y))
+        points.append((x+length//2,y+length//2))
+        
+        for point in points:
+            divide(point[0],point[1],length//2)
+        answer+=')'
+
+divide(0,0,n)
+print(answer)
