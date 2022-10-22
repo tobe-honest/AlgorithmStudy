4721e957aa717e4185ada40af755172c170e3eb8
potential1205 <potential1205@naver.com>
2022-10-22 19:45:34 +0900

    2022.10.22

---
+++ jaehoon/week8/3273_S3_두수의합.py
@@ -0,0 +1,24 @@
+import sys
+N = int(input())
+lst = list(map(int,sys.stdin.readline().split()))
+x = int(input())
+
+lst.sort()
+start,end,cnt = 0,N-1,0
+
+if N==1:
+    print(0)
+    exit()
+
+while start<end:
+    if lst[start] + lst[end] < x:
+        start+=1
+    elif lst[start] + lst[end] > x:
+        end-=1
+    elif lst[start] + lst[end] == x:
+        start+=1
+        end-=1
+        cnt+=1
+
+print(cnt)
+
