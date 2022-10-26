8dbff92ab7bab55ccc76e60ba411a33298e2ae4e
채오이 <fjdksla98@naver.com>
2022-10-26 21:37:54 +0900

    2022.10.26

--- chaewon/week9/2559_수열.py
+++ chaewon/week9/2559_수열.py
@@ -7,12 +7,9 @@ now_sum=sum(now)
 max_value=sum(now)
 
 for i in range(k,len(seq)):
-    # print(now)
     minus=now.popleft()
     now_sum-=minus
     now.append(seq[i])
-    # print(now)
     now_sum+=seq[i]
     max_value=max(now_sum, max_value)
-    # print('------',max_value)
 print(max_value)
