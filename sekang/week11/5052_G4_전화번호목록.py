079d5ff5b533bac55afee622435a48fe31e3dd89
ohsekang <assara14@naver.com>
2022-11-07 23:40:17 +0900

    2022.11.07

--- sekang/week11/5052_G4_전화번호목록.py
+++ sekang/week11/5052_G4_전화번호목록.py
@@ -1,18 +1,5 @@
 # 일관서 유지 조건 : 한 번호가 다른 번호의 접두어인 경우가 없어야 함
 
-# for _ in range(int(input())): # t
-#     n = int(input())
-#     number_list = [i for i in range(10)]
-#     flag = False
-
-#     sorted_number = sorted([input() for _ in range(n)], reverse=True)
-
-#     for number in sorted_number:
-#         j = 0
-#         cnt = 0
-#         while j < len(number):
-#             if int(number[j]) in number_list
-
 import sys
 input = sys.stdin.readline
 
