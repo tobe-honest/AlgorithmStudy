501339ef543dc3d7687b216365d53dfc9f87cc74
채오이 <fjdksla98@naver.com>
2022-12-16 17:10:22 +0900

    2022.12.16

--- chaewon/week16/1182_부분수열의합.py
+++ chaewon/week16/1182_부분수열의합.py
@@ -1,41 +1,21 @@
-# 순열 # 맞음
-n,s=map(int, input().split(' '))
-seq=list(map(int, input().split(' ')))
-
-from itertools import combinations
-answer=0
-
-for length in range(1,n+1):
-    comb = list(combinations(seq,length))
-    for c in comb:
-        if sum(c)==s:
-            answer+=1
-
-print(answer)
-
-# 백트래킹 # 시간초과
 import sys
 input=sys.stdin.readline
 n,s=map(int, input().split(' '))
 seq=list(map(int, input().split(' ')))
 answer=0
-comb=list()
-def ncr(now,arr,r,visited):
+
+def ncr(now,start):
     global answer
-    if len(now)==r and sum(now)==s:
+    if sum(now)==s and len(now)>0:
         answer+=1
+
+    if len(now)==n:
         return
     
-    now_visited=visited[:]
-    for i in range(n):
-        if not now_visited[i]:
-            now_visited[i]=True
-            now.append(arr[i])
-            ncr(now,arr,r,now_visited)
-            now.pop()
+    for i in range(start,n):
+        now.append(seq[i])
+        ncr(now[:],i+1)
+        now.pop()
 
-visited=[False]*n
-for length in range(1,n+1):
-    ncr([],seq,length,visited)
-
+ncr([],0)
 print(answer)
