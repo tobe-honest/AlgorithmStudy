1e8ebb6ade68112305143d6b681df4434e11fbc0
채오이 <fjdksla98@naver.com>
2022-12-16 18:00:18 +0900

    2022.12.16

--- chaewon/week16/87946_피로도.py
+++ chaewon/week16/87946_피로도.py
@@ -1,30 +1,30 @@
-permus=list()
-def permutation(arr,visited,r):
-    if len(arr)==r:
-        permus.append(arr)
+answer = 0
+def permutation(arr,visited,r,dungeons,k):
+    global answer
         
     for i in range(r):
         if not visited[i]:
             arr.append(i)
             visited[i]=True
-            permutation(arr[:],visited[:],r)
+            
+            is_possible, cnt = count_dungeons(arr,dungeons,k)
+            if is_possible:
+                answer=max(answer,cnt)
+                permutation(arr[:],visited[:],r,dungeons,k)
             arr.pop()
             visited[i]=False
 def count_dungeons(per, dungeons,k):
     cnt=0
-    
     for p in per:
         if k>=dungeons[p][0]:
             k-=dungeons[p][1]
             cnt+=1
         else:
-            return cnt
-    return cnt
+            return False,cnt
+    return True,cnt
 def solution(k, dungeons):
     dungeons.sort(reverse=True)
-    answer = 0
-    permutation([],[False]*len(dungeons),len(dungeons))
-    
-    for per in permus:
-        answer=max(answer,count_dungeons(per,dungeons,k))
+    global answer
+    permutation([],[False]*len(dungeons),len(dungeons),dungeons,k)
+
     return answer
